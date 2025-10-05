0b8dcdb8e6bebb8493d8fbdaca9066876fd8733f
Stefan Grujicic <stefan7822@gmail.com>
2025-10-06 01:10:07 +0200

    User CRUD, H2 test, controller, service; Eureka server

---
+++ users-service/src/main/java/com/ecommerce/usersservice/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.ecommerce.usersservice.controller;
+
+import com.ecommerce.usersservice.models.User;
+import com.ecommerce.usersservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return userService.findById(id)
+                .map(existingUser -> {
+                    existingUser.setFirstName(userDetails.getFirstName());
+                    existingUser.setLastName(userDetails.getLastName());
+                    existingUser.setEmail(userDetails.getEmail());
+                    return ResponseEntity.ok(userService.save(existingUser));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (userService.findById(id).isPresent()) {
+            userService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
--- users-service/src/main/java/com/ecommerce/usersservice/models/User.java
+++ users-service/src/main/java/com/ecommerce/usersservice/models/User.java
@@ -1,4 +1,23 @@
 package com.ecommerce.usersservice.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "app_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
 }
